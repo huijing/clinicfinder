ac39d170e414d5a9f18c913b75b52e533c5243cf
huijing <huijing@pixelonion.com>
2015-03-29 01:01:26 +0800

    Update theme for clinicfinder

--- sites/all/themes/clinicfinder/template.php
+++ sites/all/themes/clinicfinder/template.php
@@ -3,19 +3,19 @@
 /**
  * Implements hook_css_alter().
  */
-// function THEME_NAME_css_alter(&$css) { 
+function clinicfinder_css_alter(&$css) { 
 
-//   $exclude = array(
+  $exclude = array(
 
-//   //exclude core stylesheets
-//     'modules/system/SYSTEM_STYLESHEET.css' => FALSE,
+  //exclude core stylesheets
+    // 'modules/system/SYSTEM_STYLESHEET.css' => FALSE,
 
-//   //remove contrib module CSS
-//     drupal_get_path('module','MODULE_NAME').'/PATH_TO_STYLESHEET.css' => FALSE,
+  //remove contrib module CSS
+    // drupal_get_path('module','MODULE_NAME').'/PATH_TO_STYLESHEET.css' => FALSE,
 
-//   //remove contrib module CSS from libraries
-//     libraries_get_path('LIBRARY_NAME').'/PATH_TO_STYLESHEET.css' => FALSE,
-//   );
+  //remove contrib module CSS from libraries
+    libraries_get_path('DropKick') . '/css/dropkick.css' => FALSE,
+  );
 
-//   $css = array_diff_key($css, $exclude);
-// }
+  $css = array_diff_key($css, $exclude);
+}
